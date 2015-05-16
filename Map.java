074a96c905c6561534fe5c4dbf49ef5c82c3608e
PiJoules <lchan1994@yahoo.com>
2015-05-16 16:39:14 -0400

    tested first case

--- Map.java
+++ Map.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Map {
+	private static double x = 0.5, y = 0.5, totalDist = 0;
+
 	public static void main(String[] args){
 		/* Read in the values */
 		Scanner scan = new Scanner(System.in);
 		int treatCount = Integer.parseInt(scan.nextLine());
-		double[][] treats = new double[treatCount][2];
+		ArrayList<Double[]> treats = new ArrayList<>();
 		for (int i = 0; i < treatCount; i++){
 			String[] temp = scan.nextLine().split(" ");
-			treats[i][0] = Double.parseDouble(temp[0]);
-			treats[i][1] = Double.parseDouble(temp[1]);
+			Double[] treat = { Double.parseDouble(temp[0]), Double.parseDouble(temp[1]) };
+			treats.add(treat);
 		}
 
-		double x = 0.5, y = 0.5;
+		/* Brute force attempt */
+		while (treats.size() > 0){
+			/* Find the closest one then remove it */
+			double minDist = 1;
+			int index = 0;
+			double nextX = 0, nextY = 0;
+			for (int i = 0; i < treats.size(); i++){
+				double x2 = treats.get(i)[0], y2 = treats.get(i)[1];
+				double dist = Math.min( getDist(x, y, x2, y2), minDist );
+				if (dist < minDist){
+					minDist = dist;
+					index = i;
+					nextX = x2;
+					nextY = y2;
+				}
+			}
 
-		/* Check any values */
-		for (int i = 0; i < treatCount; i++){
-			System.out.println(treats[i][0] + ", " + treats[i][1]);
+			totalDist += minDist;
+			x = nextX;
+			y = nextY;
+			treats.remove(index);
 		}
+
+		System.out.println(totalDist);
+	}
+
+	/* Linear distance function */
+	private static double getDist(double x1, double y1, double x2, double y2){
+		return Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
 	}
 }
